6c707d8905275bb5c2bf4e5b17bece73b9c94c69
Daverinoe <daverinoe@gmail.com>
2022-07-10 07:32:45 +0200

    New boat class, and generalised vector positions
    Boat is now its own class, so movement is handled in the Boat class, and special methods are reserved for the extending Character/Ally boat class.
    
    Vector positions are now more generalised, to accomodate x-position-changes.

--- source/scenes/game_objects/character_boat/character_boat.gd
+++ source/scenes/game_objects/character_boat/character_boat.gd
@@ -1,94 +1,19 @@
-class_name CharacterBoat extends KinematicBody2D
+class_name CharacterBoat extends Boat
 
 # Export vars
 export (NodePath) onready var stun_timer = get_node(stun_timer) as Timer
 
-# These are blockers for the state machine transision
-# TODO: Implement this logic somehow
-var _can_move_up := true
-var _can_move_down := true
-
-# Target lane index (will be clamped)
-var _target_lane := 3
-var _current_lane := 3
-var _target_position := Vector2.ZERO setget _set_target_position
-
-# Logic for "close enough"
-const CLOSE_ENOUGH_PIXELS = 20
-var _target_reached := false
-var _position_close_enough_x := false
-var _position_close_enough_y := false
-
-# TODO: Refactor this to only affect y values
-var _lane_to_position := {  # TODO: Parameterise these to the resolution?
-	1: Vector2(90, Global.sprite_size.y*5),
-	2: Vector2(90, Global.sprite_size.y*4),
-	3: Vector2(90, Global.sprite_size.y*3),
-	4: Vector2(90, Global.sprite_size.y*2),
-	5: Vector2(90, Global.sprite_size.y),
-	6: Vector2(90, 0),
-}
-var _num_lanes := 0
-
-enum State {
-	COASTING,
-	MOVING,
-	FORCING,
-	STUNNED,  # Forced -> Stunned -> Coasting
-}
-
-var _state = State.COASTING
-
-# ENGINE CALLBACKS
 
 func _ready() -> void:
-	# Initialise position
-	_num_lanes = _lane_to_position.size() # better to use the dictionary-specific method
-	position = _lane_to_position[_current_lane]
-
+	_target_lane = 3
 	stun_timer.connect("timeout", self, "_handle_stun_timeout")
-
 
 func _process(delta: float) -> void:
 	# Explicitly handle input so that you can hold the button down to move
 	_handle_input()
 	
-	# Set a target (if none set)
-	if _current_lane != _target_lane:
-		_set_target_position(_lane_to_position[_target_lane])
-
-	# Check if we are close to the target
-	_position_close_enough_x = abs(position.x - _target_position.x) < CLOSE_ENOUGH_PIXELS
-	_position_close_enough_y = abs(position.y - _target_position.y) < CLOSE_ENOUGH_PIXELS
-	_target_reached = _position_close_enough_x and _position_close_enough_y
-
-	match _state:
-		State.COASTING:
-			if _current_lane != _target_lane:
-				# If we have a valid target set, set state to moving
-				_state = State.MOVING
-		
-		State.MOVING:
-			if not _target_reached:
-				position = lerp(position, _target_position, 0.1)
-			else:
-				# Target reached!
-				_current_lane = _target_lane
-				_state = State.COASTING
-
-		State.FORCING:
-			# This state is executed through a signal, which wipes the queue
-			# and sets the target. Enters into STUNNED state on exit
-			if not _target_reached:
-				position = lerp(position, _target_position, 0.1)
-			else:
-				# Target reached!
-				_current_lane = _target_lane
-				_state = State.STUNNED
-				stun_timer.start()
-
-		State.STUNNED:
-			pass  # Locked in this state for a beat - handled by _handle_stun_timeout()
+	if _state == State.STUNNED:
+		stun_timer.start()
 
 
 func _move_boat(normal_direction: int) -> void:
@@ -105,16 +30,6 @@ func _move_boat(normal_direction: int) -> void:
 func _handle_stun_timeout() -> void:
 	_state = State.COASTING
 
-
-# CUSTOM METHODS
-func _set_target_position(target_pos: Vector2) -> void:
-	_target_position = target_pos
-
-
-func _force_move(normal_direction: int) -> void:
-	_target_lane += normal_direction
-	_set_target_position(_lane_to_position[_target_lane])
-	_state = State.MOVING
 
 func _handle_input() -> void:
 	var _moveable = _current_lane == _target_lane and _state == State.COASTING
